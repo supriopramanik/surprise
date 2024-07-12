58ffe88c32ab39e045ddaabb721227d8a22d984d
Suprio Pramanik Emon <63463042+supriopramanik@users.noreply.github.com>
2024-07-13 01:03:01 +0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jul 24, 2024',
+  birthdate: 'Jul 10, 2024',
   name: 'SHRABU'
 };
 
